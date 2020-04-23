c24ad673e2ebdc77dd30759a756fe5f809efb9bb
maleiming <maleimings@gmail.com>
2020-04-23 11:44:41 +0800

    CH3

--- 01Bullseye/01Bullseye/ContentView.swift
+++ 01Bullseye/01Bullseye/ContentView.swift
@@ -9,20 +9,62 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     @State var alertIsVisible: Bool = false
+    @State var sliderValue: Double = 50.0
+    
     var body: some View {
+        
         VStack {
-            Text("Welcome to my first App")
-                .fontWeight(.semibold)
-                .foregroundColor(.yellow)
+            Spacer()
+
+            HStack {
+                Text("Put the bullseye as close as you can to:")
+                Text("100")
+                
+            }
+            Spacer()
+
+            
+            HStack {
+                Text("1")
+                Slider(value: self.$sliderValue, in: 1...100)
+                Text("100")
+            }
+            Spacer()
+
             Button(action: {
                 print("Button Pressed")
                 self.alertIsVisible = true
             }) {
-                Text("Hit me")
+                Text("Hit me!")
             }
             .alert(isPresented: self.$alertIsVisible) {
-                Alert(title: Text("Hello there"), message: Text("This is my first App"), dismissButton: .default(Text("Awesome!")))
+                Alert(title: Text("Hello there"),
+                      message: Text("the slider's value is \(Int(self.sliderValue.rounded()))"),
+                      dismissButton: .default(Text("Awesome!")))
+            }
+            Spacer()
+
+            
+            //score row
+            HStack {
+                Button(action: {}) {
+                    Text("Start Over")
+                }
+                Spacer()
+                
+                Text("Score:")
+                Text("999999")
+                Spacer()
+
+                Text("Round:")
+                Text("999")
+                Spacer()
+
+                Button(action: {}) {
+                    Text("Info")
+                }
             }
         }
     }
