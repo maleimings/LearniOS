af838e0e6772ea9419c0c4992bc715130c7e58ef
maleiming <maleimings@gmail.com>
2020-05-03 22:18:48 +0800

    CH27

---
+++ 04MyLocation/04MyLocation/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  04MyLocation
+//
+//  Created by Valiant on 2020/5/3.
+//  Copyright © 2020 Valiant. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
