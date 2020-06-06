2d2c403e07786a9b15c43d721585c7ddadf2c4be
maleiming <maleimings@gmail.com>
2020-06-06 22:01:33 +0800

    CH44

--- StoreSearch/StoreSearch/LandscapeViewController.swift
+++ StoreSearch/StoreSearch/LandscapeViewController.swift
@@ -15,6 +15,13 @@ class LandscapeViewController: UIViewController {
     
     var searchResults = [SearchResult]()
     private var firstTime = true
+    private var downloads = [URLSessionDownloadTask]()
+    
+    deinit {
+        for task in downloads {
+            task.cancel()
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -50,7 +57,11 @@ class LandscapeViewController: UIViewController {
     }
     
     @IBAction func pageChanged(_ sender: UIPageControl) {
-        scrollView.contentOffset = CGPoint(x: scrollView.bounds.size.width * CGFloat(sender.currentPage), y: 0)
+        UIView.animate(withDuration: 0.3, delay: 0,
+                       options: [.curveEaseInOut], animations: {
+            self.scrollView.contentOffset = CGPoint(
+                x: self.scrollView.bounds.size.width * CGFloat(sender.currentPage), y: 0) },
+          completion: nil)
     }
     
     private func tileButtons(_ searchResults: [SearchResult]) {
@@ -99,9 +110,9 @@ class LandscapeViewController: UIViewController {
         var column = 0
         var x = marginX
         for (index, result) in searchResults.enumerated() {
-            let button = UIButton(type: .system)
-            button.backgroundColor = UIColor.white
-            button.setTitle("\(index)", for: .normal)
+            let button = UIButton(type: .custom)
+            button.setBackgroundImage(UIImage(named: "LandscapeButton"), for: .normal)
+            downloadImage(for: result, andPlaceOn: button)
             
             button.frame = CGRect(x: x+paddingHorz, y: marginY + CGFloat(row)*itemHeight + paddingVert, width: buttonWidth, height: buttonHeight)
             
@@ -130,6 +141,24 @@ class LandscapeViewController: UIViewController {
         pageControl.currentPage = 0
     }
 
+    private func downloadImage(for searchResult: SearchResult, andPlaceOn button: UIButton) {
+        if let url = URL(string: searchResult.imageSmall) {
+            let task = URLSession.shared.downloadTask(with: url) {
+                [weak button] url, response, error in
+                if error == nil, let url = url,
+                let data = try? Data(contentsOf: url),
+                    let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        if let button = button {
+                            button.setImage(image, for: .normal)
+                        }
+                    }
+                }
+            }
+            task.resume()
+            downloads.append(task)
+        }
+    }
 }
 
 extension LandscapeViewController: UIScrollViewDelegate {
